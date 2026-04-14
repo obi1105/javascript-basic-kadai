dc9358f68704bf6abdd82dea702f0591390c17dd
obi1105 <obi1105@e-corporation.co.jp>
2026-04-14 14:53:57 +0900

    kadai_mdn_jsをプッシュ

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,12 +1,12 @@
 const today = new Date();
 
-const [month, day, year] = [
+const [year, month, day] = [
     today.getMonth() +1,
     today.getDate(),
     today.getFullYear(),
 ];
 
-console.log(year,month,day,);
+console.log(year,'年',month,'月',day,'日');
 
 
 
